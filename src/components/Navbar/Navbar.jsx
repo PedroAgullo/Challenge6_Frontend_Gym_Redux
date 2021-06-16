330ab4e13f8a8bc9b836470742c930e824a1f810
mlfernandez <mlfernandez@gmail.com>
2021-06-16 22:42:06 +0200

    creating folder and files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,79 @@
 import React from 'react';
 import "./Navbar.css";
-import {NavLink} from 'react-router-dom';
-import Logo from '../../images/logotrans.png';
-
+import { NavLink } from 'react-router-dom';
+import Logo from '../../images/logofinalnuevogrisSmall.png';
+import { connect } from 'react-redux';
+import { LOGOUT, LOGOUTROOM } from '../../redux/types';
+import { useHistory } from 'react-router-dom';
 
 const Navbar = (props) => {
 
+
+  let history = useHistory();
+
+
+  const takeMe = (where) => {
+    history.push(where);
+  }
+
+  const logOut = () => {
+    props.dispatch({ type: LOGOUT });
+    props.dispatch({ type: LOGOUTROOM });
+
+    setTimeout(() => {
+      history.push('/');
+    }, 500)
+
+  }
+
+  if (props.credentials?.token == '') {
     return (
-      <>
-        <div className="nav">
+      <div className="nav">
+        <div className="logo">
+          <NavLink to="/"><img className="img" src={Logo} /></NavLink>
+        </div>
 
-          <div className="logo">
-            <NavLink to="/"><img className="img"  src={Logo}/></NavLink>
-            
+        <div className="blank"></div>
+
+        <div className="NavMenu" >
+          <div className="NavLink">
+            <NavLink style={{ color: 'inherit', textDecoration: 'inherit'}} to="/register">Inscribirse</NavLink>
           </div>
+          <div className="NavLink" activeClassName="selected">
+          <NavLink style={{ color: 'inherit', textDecoration: 'inherit' }} to="/login">Login</NavLink>
+          </div>
+        </div>
+      </div>
+    )
 
-         <div className="blank"></div>
-
-         
-          <div className="NavMenu">
-{/*             <div className="NavLink">
-              <NavLink style={{ color: 'inherit', textDecoration: 'inherit'}} to="/contacto">Contacto</NavLink>
-            </div> */}
+  } else {
+    return (
+      <div className="nav">
+        <div className="logo">
+          <NavLink to="/"><img className="img" src={Logo} /></NavLink>
+        </div>
+        <div className="blank"></div>
+        <div className="NavMenu">
+          <div className="NavLink">
             <div className="NavLink">
-              <NavLink style={{ color: 'inherit', textDecoration: 'inherit'}} to="/register">Inscribirse</NavLink>
+              <NavLink style={{ color: 'inherit', textDecoration: 'inherit' }} onClick={()=>logOut()} to="/">Logout</NavLink>
             </div>
             <div className="NavLink" activeClassName="selected">
-              <NavLink style={{ color: 'inherit', textDecoration: 'inherit'}} to="/login">Login</NavLink>
+              <NavLink style={{ color: 'inherit', textDecoration: 'inherit' }} to="/profile">Profile</NavLink>
             </div>
-            
-            
-            
-    
-          </div>  
-
+            <div className="NavLink" activeClassName="selected">
+              <NavLink style={{ color: 'inherit', textDecoration: 'inherit' }} to="/Useroom">Clases</NavLink>
+            </div>
+          </div>
         </div>
-      </>
-    );
-  };
-  export default Navbar;
+      </div>
+        )
+}
+
+
+};
+
+
+export default connect((state)=>({credentials:state.credentials}))(Navbar);
+
+
